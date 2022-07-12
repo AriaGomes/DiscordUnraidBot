a5e4f750adff4720a0437bcac8a4887454571e86
AriaGomes <aria.gomes00@gmail.com>
2022-07-12 04:26:07 -0400

    Added more docker functionality, fix embeds

--- commands/docker.js
+++ commands/docker.js
@@ -1,5 +1,6 @@
 const {SlashCommandBuilder} = require('@discordjs/builders');
-const {API_URL} = require('../config.json');
+const {MessageEmbed} = require('discord.js');
+const {API_URL, dockerImageURL} = require('../config.json');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -9,8 +10,20 @@ module.exports = {
     const response = await fetch(API_URL + 'getServers');
     let data = await response.json();
     data = data.servers['https://192-168-1-20.bbcd0582571cd2ef8b7cc59bc12bc8802251ab3a.myunraid.net/'].docker.details.containers;
-    console.log(data);
+    const arr = [];
+    for ( let i = 0; i < Object.keys(data).length; i++) {
+      const title = data[Object.keys(data)[i]].name;
+      const status = data[Object.keys(data)[i]].status;
+      const id = data[Object.keys(data)[i]].containerId;
+      const imageURL = dockerImageURL + data[Object.keys(data)[i]].imageUrl;
 
-    await interaction.reply(`🏓 ${Math.round(interaction.client.ws.ping)}ms`);
+      const embed = new MessageEmbed()
+          .setTitle(title)
+          .setThumbnail(imageURL);
+      arr.push(embed);
+    }
+
+    console.log(arr);
+    await interaction.reply({embeds: [arr[0]]});
   },
 };
